ba05f27b6cb5bf3fc20ccd83407a7c5c891f16d6
Nicholas <tao.nicholas1@gmail.com>
2020-09-06 12:17:46 -0400

    setup frontend and login form

---
+++ client/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Login from "./Login";
+function App() {
+  const [id, setId] = useState();
+  return (
+    <>
+      {id}
+      <Login onIdSubmit={setId} />
+    </>
+  );
+}
+
+export default App;
