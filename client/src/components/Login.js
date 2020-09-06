c51df369c3a25bd73f01ba5982f2d3bdee13669b
Nicholas <tao.nicholas1@gmail.com>
2020-09-06 12:54:06 -0400

    create user id button, finish login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,6 @@
 import React, { useRef } from "react";
 import { Container, Form, Button } from "react-bootstrap";
+import { v4 as uuidV4 } from "uuid";
 
 export default function Login({ onIdSubmit }) {
   const idRef = useRef();
@@ -7,6 +8,10 @@ export default function Login({ onIdSubmit }) {
   function handleSubmit(e) {
     e.preventDefault(); //prevents page from refreshing
     onIdSubmit(idRef.current.value);
+  }
+
+  function createNewId() {
+    onIdSubmit(uuidV4());
   }
   return (
     <Container
@@ -21,7 +26,9 @@ export default function Login({ onIdSubmit }) {
         <Button type="submit" className="mr-2">
           Login
         </Button>
-        <Button variant="secondary">Create a New Id</Button>
+        <Button onClick={createNewId} variant="secondary">
+          Create a New Id
+        </Button>
       </Form>
     </Container>
   );
