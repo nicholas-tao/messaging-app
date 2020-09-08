006d8fec4e4215d2c1a35e07e9cb35b6d4274640
Nicholas <tao.nicholas1@gmail.com>
2020-09-08 01:02:34 -0400

    works now i think its done

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,8 +5,10 @@ import Dashboard from "./Dashboard";
 import { ContactsProvider } from "../contexts/ContactsProvider";
 import { ConversationsProvider } from "../contexts/ConversationsProvider";
 import { SocketProvider } from "../contexts/SocketProvider";
+
 function App() {
   const [id, setId] = useLocalStorage("id");
+
   const dashboard = (
     <SocketProvider id={id}>
       <ContactsProvider>
@@ -16,6 +18,7 @@ function App() {
       </ContactsProvider>
     </SocketProvider>
   );
+
   return id ? dashboard : <Login onIdSubmit={setId} />;
 }
 
